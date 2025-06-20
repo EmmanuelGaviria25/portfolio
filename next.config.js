8e036f00302febed6513e13cf43846ded8f1dbaa
Emmanuel Gaviria <emmanuel.gaviria.25@gmail.com>
2025-06-20 11:16:10 -0500

    ajuste de configuración

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@
 const nextConfig = {
     output: 'export',
     distDir: 'dist',
+    images: {
+        unoptimized: true,
+    },
+    reactStrictMode: true,
     env: {
         TEMPLATE_ID : process.env.TEMPLATE_ID,
         SERVICE_ID : process.env.SERVICE_ID,
