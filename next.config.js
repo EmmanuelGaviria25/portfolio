8ce04ac6a920f45b95e54add2e59cbe68688efc1
Emmanuel Gaviria <emmanuel.gaviria.25@gmail.com>
2025-06-20 11:09:43 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+    output: 'export',
     env: {
         TEMPLATE_ID : process.env.TEMPLATE_ID,
         SERVICE_ID : process.env.SERVICE_ID,
