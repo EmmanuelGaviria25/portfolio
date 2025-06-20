bb2ddcb659113f8975c9ae536ff4ec6832a9ce66
Emmanuel Gaviria <emmanuel.gaviria.25@gmail.com>
2025-06-20 11:04:21 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
     env: {
         TEMPLATE_ID : process.env.TEMPLATE_ID,
         SERVICE_ID : process.env.SERVICE_ID,
         PUBLIC_KEY : process.env.PUBLIC_KEY,
-    },
-    basePath: isProd ? '/portfolio' : '',
-    assetPrefix: isProd ? '/portfolio/' : '',
+    }
 }
 
 module.exports = nextConfig
